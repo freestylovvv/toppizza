36033b22f0fc0d7212513ea2a2f584943121d1f1
freestylovvv <svyatoslovvv@gmail.com>
2026-03-16 18:51:59 +0500

    fix hydration: portal modal + suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   )
 }
