4b6719f371b4260371c82a7c38e9768ab448ff3b
freestylovvv <svyatoslovvv@gmail.com>
2026-04-02 16:51:35 +0500

    fix: sms sender url encode

--- lib/sms.ts
+++ lib/sms.ts
@@ -1,6 +1,6 @@
 export async function sendSms(phone: string, message: string) {
   const cleanPhone = phone.replace(/\D/g, '')
-  const url = `https://sms.ru/sms/send?api_id=${process.env.SMSRU_API_KEY}&to=${cleanPhone}&msg=${encodeURIComponent(message)}&from=${process.env.SMSRU_SENDER || 'sms'}&json=1`
+  const url = `https://sms.ru/sms/send?api_id=${process.env.SMSRU_API_KEY}&to=${cleanPhone}&msg=${encodeURIComponent(message)}&from=${encodeURIComponent(process.env.SMSRU_SENDER || '')}&json=1`
   const res = await fetch(url)
   const data = await res.json()
   if (data.status !== 'OK') {
